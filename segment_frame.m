06c075fa17de929159c1328fc0c919d7215d34c9
AladdinKaan <45555083+AladdinKaan@users.noreply.github.com>
2021-01-07 14:42:40 +0100

    Add files via upload
    better segmentation algorithm, elliminates varying background noise

--- segment_frame.m
+++ segment_frame.m
@@ -1,17 +1,20 @@
 
-function [centroids] = segment_frame(imageData)
-%Removing the median value for background variation
-imageData = imageData - median(imageData(:));
+function [centroids] = segment_frame(imageData,oldpixelsize)
+
+b = 1200/oldpixelsize;  %block size in pixels for removing noise
+gauss_sigma = 100/oldpixelsize;
+
+%Removing the median value locally on a [b x b] grid for noise elimination
+fun = @(block_struct) block_struct.data - median(block_struct.data(:));
+
+data = blockproc(imageData,[b,b],fun);
 
 %filtering the image with a gaussian with sigma should be 1.5~2
-image_f = imgaussfilt(imageData,3/2);
+image_f = imgaussfilt(data,gauss_sigma);
 
-%std of the data for thresholding
-%sigma_f = std(image_f(:));
-sigma = std(imageData(:));
-
-%high std should have higher threshold
-thr = 3*sigma;  %should be sigma_f?
+%std of the data for thresholding, high std should have higher threshold
+sigma = std(data(:));
+thr = 3*sigma; 
 
 % thresholding pixel values based on median
 segmented_im = image_f.*(image_f>thr);
